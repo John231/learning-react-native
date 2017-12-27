bc285ff19d8cc61a93ce80eaef93092722329c73
John231 <j_b_93@hotmail.co.uk>
2017-12-27 21:18:39 +0100

    Lesson 34 - added in a network request to AlbumList.js

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import axios from "axios";
 
 class AlbumList extends Component {
   componentWillMount() {
-    console.log("Component will mount in Album List");
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => {
+        console.log(response);
+      });
   }
   render() {
     return (
